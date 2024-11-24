1f89b0e90418ad08d4231ed16c79528aef2001fc
FaczanP <66326287+FaczanP@users.noreply.github.com>
2024-11-24 17:35:40 +0100

    End of section 6

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -11,6 +12,7 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent {
   private accountService = inject(AccountService);
+  private toastr = inject(ToastrService);
   // usersFromHomeComponent = input.required<any>();   @Input() usersFromHomeComponent: any; It works before Angular 17.1 it was changed with a newer version
   cancelRegister = output<boolean>();     // @Output() cancelRegister = new EventEmitter(); It works before Angular 17.1 it was changed with a newer version
   model: any = {}
@@ -22,7 +24,8 @@ export class RegisterComponent {
         console.log(response);
         this.cancel();
       },
-      error: error => console.log(error)
+      error: error => this.toastr.error(error.error)
+        // console.log(error)
     })
   }
 
