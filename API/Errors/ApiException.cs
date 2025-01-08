742348c5dc2149ec5a9b3001ed0ce53b50b9119a
FaczanP <66326287+FaczanP@users.noreply.github.com>
2025-01-08 20:37:20 +0100

    End of section 9

--- API/Errors/ApiException.cs
+++ API/Errors/ApiException.cs
@@ -4,5 +4,5 @@ public class ApiException(int statusCode, string message, string? details)
 {
     public int StatusCode { get; set; } = statusCode;
     public string Message { get; set; } = message;
-    public string Details { get; set; } = details;
+    public string? Details { get; set; } = details;
 }
